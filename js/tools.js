13c1303194425d369bde4a64ff5fbe51bdaa2639
htmltest <struev.vs@gmail.com>
2018-04-17 14:09:38 +0300

    content pages

--- js/tools.js
+++ js/tools.js
@@ -24,4 +24,39 @@ $(document).ready(function() {
         e.preventDefault();
     });
 
+    $('.distrib-map-list-item').click(function() {
+        var curItem = $(this);
+        var curID = curItem.data('id');
+        $('.distrib-map-list-item.active').removeClass('active');
+        curItem.addClass('active');
+        $('.distrib-map-scheme-point.active').removeClass('active');
+        $('.distrib-map-scheme-point[data-id="' + curID + '"]').addClass('active');
+    });
+
+    $('.distrib-map-scheme-point').hover(
+        function() {
+            var curPoint = $(this);
+            var curID = curPoint.data('id');
+            $('.distrib-map-list-item[data-id="' + curID + '"]').addClass('hover');
+        },
+
+        function() {
+            $('.distrib-map-list-item.hover').removeClass('hover');
+        }
+    );
+
+    $('.distrib-map-menu ul li a').click(function(e) {
+        var curItem = $(this).parent();
+        if (!curItem.hasClass('active')) {
+            $('.distrib-map-menu ul li.active').removeClass('active');
+            curItem.addClass('active');
+            var curIndex = $('.distrib-map-menu ul li').index(curItem);
+            $('.distrib-map-tab.active').removeClass('active');
+            $('.distrib-map-tab').eq(curIndex).addClass('active');
+        }
+        e.preventDefault();
+    });
+
+    $('.distrib-map-tab-list').jScrollPane({autoReinitialise: true, verticalDragMinHeight: 60});
+
 });
